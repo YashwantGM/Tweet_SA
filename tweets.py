a2c9295c09e4a285b99dc2b96a8983a293b89038
YashwantGM <yashgm96@gmail.com>
2018-08-22 11:23:18 +0530

    Initial version of Tweets.py

---
+++ tweets.py
@@ -0,0 +1,88 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+class TwitterClient(object):
+	'''
+	Generic Twitter Class for sentiment analysis.
+	'''
+	def __init__(self):
+		'''
+		Class constructor or initialization method.
+		'''
+		# keys and tokens from the Twitter Dev Console
+		consumer_key = 'OZR1Y6n7TZQNpu7XWrsMKdd0Y'
+		consumer_secret = 'dKkCUXQ8hFfnvZ140Vh0ZtXqyZfcqROCZSLiv7QuOuRZbA4md6'
+		access_token = '785131231986016256-uEkBDNq22V6mDKaHeK8ExXEtmx9y2Ib'
+		access_token_secret = 'kYvpcFXVpfWQVfdQumg45r3b5xf3FtjiawHeQZzGfJVLY'
+
+		# attempt authentication
+		try:
+			# create OAuthHandler object
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			# set access token and secret
+			self.auth.set_access_token(access_token, access_token_secret)
+			# create tweepy API object to fetch tweets
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
+	def get_tweets(self, query, count = 10):
+		'''
+		Main function to fetch tweets and parse them.
+		'''
+		# empty list to store parsed tweets
+		tweets = []
+
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search(q = query, count = count)
+
+			# parsing tweets one by one
+			for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+				parsed_tweet = {}
+
+				# saving text of tweet
+				parsed_tweet['text'] = tweet.text
+				# saving sentiment of tweet
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+				# appending parsed tweet to tweets list
+				if tweet.retweet_count > 0:
+					# if tweet has retweets, ensure that it is appended only once
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
+
+			# return parsed tweets
+			return tweets
+
+		except tweepy.TweepError as e:
+			# print error (if any)
+			print("Error : " + str(e))
+
+
+def main():
+	# creating object of TwitterClient Class
+	api = TwitterClient()
+	# calling function to get tweets
+	tweets = api.get_tweets(query = 'Donald Trump', count = 200)
+
+# picking positive tweets from tweets
+	ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+	# percentage of positive tweets
+	print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+	# picking negative tweets from tweets
+	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+	# percentage of negative tweets
+	print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+	# percentage of neutral tweets
+	print("Neutral tweets percentage: {} % \
+		".format(100*len(tweets - ntweets - ptweets)/len(tweets)))
+
+
+if __name__ == "__main__":
+	# calling main function
+	main()
